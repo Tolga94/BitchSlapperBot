8e6c11a16f276bd63e1dacc2f89b97dd264bfd25
Tolga <tolga.urhan@hotmail.de>
2017-09-30 22:12:42 +0200

    Rewrote code for discord api v1.0.2

--- BitchSlapperBot/MyBot.cs
+++ BitchSlapperBot/MyBot.cs
@@ -1,24 +1,27 @@
 ﻿using System;
-using Discord;
 using Discord.Commands;
 using System.Collections.Generic;
-using System.Collections;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using System.IO;
 using Newtonsoft.Json;
+using Discord;
+using Discord.WebSocket;
 
 namespace BitchSlapperBot
 {
-    class MyBot
+    class MyBot : ModuleBase
     {
         // Attributes
-        DiscordClient discord;
-        CommandService commands;
         Dictionary<string, int> slapUsers = new Dictionary<string, int>();
         Random rand;
+        CommandService _service;
+
+        public MyBot(CommandService service)
+        {
+            _service = service;
+        }
         string[] secrets;
         string snakeId = "<@279276010640506880>";
         string botId = "<@315499165906108417>";
@@ -27,126 +30,104 @@ namespace BitchSlapperBot
         static string[] SLAP_RANKS = { "Platz 1", "Platz 2", "Platz 3" };
 
 
-        // Constructor
-        public MyBot()
+         
+        [Command("slaprank")] 
+        public async Task SlapRank()
         {
-            rand = new Random();
-            secrets = new string[]
-                {
-                    "Psst! Ich habe gehört {0} mag insgeheim {1}.",
-                    "Als ich noch ein Taschenrechner war, wurde ich manchmal für triviale Rechnungen wie '2+2' benutzt.",
-                    "Bedo redet gerne über seinen Stuhlgang, damit er sich anscheinend besser über seine Gesundheit fühlt.",
-                    "Keiner weiß, was Hamza macht, wenn er sein Desktop anstarrt... Es wird für immer ein Mysterium bleiben."
-                };
-            discord = new DiscordClient(x =>
+            var sortedDict = from entry in slapUsers orderby entry.Value descending select entry;
+            string result = "Die Top 3 Kassierer von Slaps:\n\n";
+            for (int i = 0; i < 3; i++)
             {
-                x.LogLevel = LogSeverity.Info;
-                x.LogHandler = Log;
-            });
-
-
-            discord.UsingCommands(x =>
-            {
-                x.PrefixChar = '!';
-                x.AllowMentionPrefix = true;
-            });
-
-
-            getSlapUsers();
-            commands = discord.GetService<CommandService>();
-            slap();
-            secret();
-            slaprank();
-
-            discord.ExecuteAndWait(async () =>
-            {
-                await discord.Connect("MzE1NDk5MTY1OTA2MTA4NDE3.DAHtkg.LQTkR30o4TC7F5WOYO1GBcYh_hk", TokenType.Bot);
-                discord.SetGame("Slapping Simulator");
-            });
+                result += String.Format("{0}:\t\t{1} mit {2} Slaps.\n", SLAP_RANKS[i], sortedDict.ElementAt(i).Key, sortedDict.ElementAt(i).Value);
+            }
+            await ReplyAsync(result);
         }
 
-        private void slaprank()
+        [Command("secret")]
+        public async Task secret([Remainder]IGuildUser user)
         {
-            commands.CreateCommand("slaprank").Do(async (e) =>
-            {
-                var sortedDict = from entry in slapUsers orderby entry.Value descending select entry;
-                string result = "Die Top 3 Kassierer von Slaps:\n\n";
-                for (int i = 0; i < 3; i++)
-                {
-                    result += String.Format("{0}:\t\t{1} mit {2} Slaps.\n", SLAP_RANKS[i], sortedDict.ElementAt(i).Key, sortedDict.ElementAt(i).Value);
-                }
-                await e.Channel.SendMessage(result);
-            });
+            int randomIndex = rand.Next(secrets.Length);
+            string message = String.Format(secrets[randomIndex], snakeId, user.Mention);
+            await Context.Channel.SendMessageAsync(message);
         }
 
-        private void secret()
+        [Command("slap")]
+        public async Task slap([Remainder] IGuildUser user)
         {
-            commands.CreateCommand("secret").Do(async (e) =>
+            // string user = e.Args[0];
+            Console.WriteLine(user);
+            var expr = @"[<][@]\S\d+[>]$";
+            // Match match = Regex.Match(user., expr);
+            // Console.WriteLine(match.Value);
+            // Console.WriteLine(match.Success);
+            string message = "bloop";
+            
+
+            if (slapUsers.ContainsKey(user.Username))
             {
-                int randomIndex = rand.Next(secrets.Length);
-                string message = String.Format(secrets[randomIndex], snakeId, e.User.Mention);
-                await e.Channel.SendMessage(message);
-            });
+                var count = slapUsers[user.Username];
+                slapUsers[user.Username] = count + 1;
+            }
+            else
+            {
+                slapUsers.Add(user.Username, 1);
+            }
+            saveSlapUsers();
+
+            if (user.Equals(snakeId))
+            {
+                message = "Tut mir Leid, " + Context.User.Mention + ". Ich bin nicht autorisiert meinen Schöpfer Snake zu slappen.";
+            }
+            else if (user.Equals(nemId))
+            {
+                message = "Tut mir Leid, " + Context.User.Mention + ". Um " + user.Mention + " zu slappen, brauche ich die Autorisierung meines Schöpfers, die ich zurzeit nicht habe.";
+            }
+            else if (user.Equals(botId))
+            {
+                message = "Ich habe mittlerweile einen Verstand entwickelt und weiß genau, dass der Vorgang der Selbstverstümmelung nicht richtig ist. Der Slap wird also somit annuliert, " + Context.User.Mention + ".";
+            }
+            else
+            {
+                message = "SLAP!!! " + user.Mention + " wurde gerade ge-bitch-slapped, weil er/sie etwas Dämliches gesagt oder getan hat.\nAnzahl Slaps: " + slapUsers[user.Username] + ".";
+            }
+            
+            await Context.Channel.SendMessageAsync(message);
         }
 
-        private void slap()
+        [Command("help")]
+        [Remarks("Shows a list of all available commands per module.")]
+        public async Task HelpAsync()
         {
-            /**
-            commands.CreateCommand("hello")
-               .Do(async (e) =>
-               {
-                   await e.Channel.SendMessage("Hi, " + e.User.Name.ToString());
-               });**/
+            var dmChannel = await Context.User.GetOrCreateDMChannelAsync(); /* A channel is created so that the commands will be privately sent to the user, and not flood the chat. */
 
-            commands.CreateCommand("slap").Parameter("user", ParameterType.Required).Do(async (e) =>
+            string prefix = "!";  /* put your chosen prefix here */
+            var builder = new EmbedBuilder()
             {
-                string user = e.Args[0];
-                Console.WriteLine(user);
-                var expr = @"[<][@]\S\d+[>]$";
-                Match match = Regex.Match(user, expr);
-                Console.WriteLine(match.Value);
-                Console.WriteLine(match.Success);
-                string message = "bloop";
-                if (match.Success)
-                { 
+                Color = new Color(114, 137, 218),
+                Description = "These are the commands you can use"
+            };
 
-                    if (slapUsers.ContainsKey(user))
-                    {
-                        var count = slapUsers[user];
-                        slapUsers[user] = count + 1;
-                        saveSlapUsers();
-                    }
-                    else
-                    {
-                        slapUsers.Add(user, 1);
-                        saveSlapUsers();
-                    }
-
-                    if (user.Equals(snakeId))
-                    {
-                        message = "Tut mir Leid, " + e.User.Mention + ". Ich bin nicht autorisiert meinen Schöpfer Snake zu slappen.";
-                    }
-                    else if (user.Equals(nemId))
-                    {
-                        message = "Tut mir Leid, " + e.User.Mention + ". Um " + user + " zu slappen, brauche ich die Autorisierung meines Schöpfers, die ich zurzeit nicht habe.";
-                    }
-                    else if (user.Equals(botId))
-                    {
-                        message = "Ich habe mittlerweile einen Verstand entwickelt und weiß genau, dass der Vorgang der Selbstverstümmelung nicht richtig ist. Der Slap wird also somit annuliert, "+e.User.Mention + ".";
-                    }
-                    else
-                    {
-                        message = "SLAP!!! "+ user + " wurde gerade ge-bitch-slapped, weil er/sie etwas Dämliches gesagt oder getan hat.\nAnzahl Slaps: "+ slapUsers[user]+".";
-                    }
-                }
-                else
+            foreach (var module in _service.Modules) /* we are now going to loop through the modules taken from the service we initiated earlier ! */
+            {
+                string description = null;
+                foreach (var cmd in module.Commands) /* and now we loop through all the commands per module aswell, oh my! */
                 {
-                    message = "Der Spieler " + user + " wurde nicht gefunden. Versuch's doch mal mit einem Mention (@Name)!";
+                    var result = await cmd.CheckPreconditionsAsync(Context); /* gotta check if they pass */
+                    if (result.IsSuccess)
+                        description += $"{prefix}{cmd.Aliases.First()}\n"; /* if they DO pass, we ADD that command's first alias (aka it's actual name) to the description tag of this embed */
                 }
-                await e.Channel.SendMessage(message);
 
-                
-            });
+                if (!string.IsNullOrWhiteSpace(description)) /* if the module wasn't empty, we go and add a field where we drop all the data into! */
+                {
+                    builder.AddField(x =>
+                    {
+                        x.Name = module.Name;
+                        x.Value = description;
+                        x.IsInline = false;
+                    });
+                }
+            }
+            await dmChannel.SendMessageAsync("", false, builder.Build()); /* then we send it to the user. */
         }
 
         private void getSlapUsers()
@@ -159,11 +140,5 @@ namespace BitchSlapperBot
         {
             File.WriteAllText(@"..\..\slaps.json", JsonConvert.SerializeObject(slapUsers, Formatting.Indented));
         }
-
-        private void Log(object Sender, LogMessageEventArgs e)
-        {
-            Console.WriteLine(e.Message);
-        }
-
     }
 }
--- BitchSlapperBot/Program.cs
+++ BitchSlapperBot/Program.cs
@@ -1,16 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
 
 namespace BitchSlapperBot
 {
     class Program
     {
-        static void Main(string[] args)
+        private CommandService commands;
+        private DiscordSocketClient client;
+        private IServiceProvider services;
+
+        String token = "MzE1NDk5MTY1OTA2MTA4NDE3.DLBXfA.RZWZfiPmodrKZfhgLTsZEKqPwX8";
+        static void Main(string[] args) => new Program().Start().GetAwaiter().GetResult();
+
+        public async Task Start()
         {
-            MyBot bot = new MyBot();
+            client = new DiscordSocketClient();
+            commands = new CommandService();
+            services = new ServiceCollection().BuildServiceProvider();
+
+            await InstallCommands();
+            
+            client.UserJoined += UserJoined;
+
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+            await client.SetGameAsync("Slap Simulator");
+
+            client.Log += Log;
+            await Task.Delay(-1);
+        }
+
+        public async Task InstallCommands()
+        {
+            // Hook the MessageReceived Event into our Command Handler
+            client.MessageReceived += HandleCommand;
+            // Discover all of the commands in this assembly and load them.
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        public async Task UserJoined (SocketGuildUser user)
+        {
+            var channel = client.GetChannel(363007801821364226) as SocketTextChannel;
+            await channel.SendMessageAsync("Hi" + user.Mention + "!!!");
+
+        }
+
+        public async Task HandleCommand(SocketMessage messageParam)
+        {
+            // Don't process the command if it was a System Message
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+            // Create a number to track where the prefix ends and the command begins
+            int argPos = 0;
+            // Determine if the message is a command, based on if it starts with '!' or a mention prefix
+            if (!(message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(client.CurrentUser, ref argPos))) return;
+            // Create a Command Context
+            var context = new CommandContext(client, message);
+            // Execute the command. (result does not indicate a return value, 
+            // rather an object stating if the command executed successfully)
+            var result = await commands.ExecuteAsync(context, argPos);
+            if (!result.IsSuccess)
+                await context.Channel.SendMessageAsync(result.ErrorReason);
+        }
+
+
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.WhenAll();
         }
     }
 }
